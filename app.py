463262ed2817e16dcfb44edf6185fc749431113b
Nate Goldman <427322+ungoldman@users.noreply.github.com>
2024-01-16 09:51:25 -0800

    add types example

--- app.py
+++ app.py
@@ -1,9 +1,26 @@
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, world!'
+
+@app.route('/types')
+def data_types():
+    str = 'quarks'
+    num = 42
+    flt = 2.3
+    lst = [str, num, flt]
+    # no shorthand notation in python
+    dct = {
+        'string': str,
+        'number': num,
+        'float': flt,
+        'list': lst
+    }
+    return {
+        'dictionary': dct
+    }
 
 if __name__ == '__main__':
     app.run(debug = True, host = '0.0.0.0', port = 5001)
